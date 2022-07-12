8907551039bf4446cdce82c2c2930371297c14fa
owais <owaes.hamouda.2000@gmail.com>
2022-07-12 18:17:43 +0300

    Layout + appBar

--- lib/views/pages/overview_page.dart
+++ lib/views/pages/overview_page.dart
@@ -1,10 +1,32 @@
+import 'package:dashboard/controllers/overview_controller.dart';
+import 'package:dashboard/views/widgets/app/responsiveness.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class OverviewPage extends StatelessWidget {
-  const OverviewPage({Key? key}) : super(key: key);
+class OverViewPage extends StatelessWidget {
+  OverViewPage({Key? key}) : super(key: key);
+  final OverViewController overViewController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Navigator.defaultRouteName
+    return ResponsiveWidget(
+      large: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: const Color(0xFF282828),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(),
+          ),
+        ],
+      ),
+      small: Container(
+        constraints: const BoxConstraints.expand(),
+      ),
+    );
   }
 }
